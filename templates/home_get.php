cea504cb6667ad604ba778a73171ceaeb65f0645
Ammanat2004 <66011212061@msu.ac.th>
2025-02-28 22:55:30 +0700

    Update home_get.php

--- templates/home_get.php
+++ templates/home_get.php
@@ -3,3 +3,18 @@
   <!-- if searchbox send to search_get  in route-->
 <!-- if click register send to register get in route -->
  <!-- if click login send to login get in route -->
+
+ <!DOCTYPE html>
+<html lang="th">
+  
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>ยินดีต้อนรับ</title>
+
+</head>
+<body>
+    <h2>Wellcome Baby.
+    </h2>
+</body>
+</html>
