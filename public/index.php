adebaa3e914c2bdaccf7c3f18a8591b1eb6a8883
PhatthadonBS <phattadon232@gmail.com>
2025-02-28 12:57:58 +0700

    update

---
+++ public/index.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+
+const INCLUDES_DIR = __DIR__ . '/../includes';
+const ROUTE_DIR = __DIR__ . '/../routes';
+const TEMPLATES_DIR = __DIR__ . '/../templates';
+const DATABASE_DIR = __DIR__ . '/../databases';
+
+session_start();
+
+
+require_once INCLUDES_DIR . '/router.php';
+require_once INCLUDES_DIR . '/view.php';
+require_once INCLUDES_DIR . '/db.php';
+
+
+const PUBLIC_ROUTES = ['/', '/login', '/register'];
+
+if (in_array(strtolower($_SERVER['REQUEST_URI']), PUBLIC_ROUTES)) {
+    dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+    exit;
+} elseif (isset($_SESSION['timestamp']) && time() - $_SESSION['timestamp'] < 30 * 60) {
+
+    $unix_timestamp = time();
+    $_SESSION['timestamp'] = $unix_timestamp;
+    dispatch($_SERVER['REQUEST_URI'], $_SERVER['REQUEST_METHOD']);
+} else {
+    unset($_SESSION['timestamp']);
+    header('Location: /');
+    exit;
+}
