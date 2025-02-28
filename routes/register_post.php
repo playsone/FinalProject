459635b9c7a10c3622bad603caaf02fd82e370c2
Ammanat2004 <66011212061@msu.ac.th>
2025-02-28 22:36:05 +0700

    Update register_post.php

--- routes/register_post.php
+++ routes/register_post.php
@@ -3,6 +3,28 @@
 <!-- send to register get in template-->
 <!-- check duplicate username  hash password  check duplicate email -->
 
+<?php
+    if(isset($_POST["submit"])){
+        if($_POST["pwd"] !== $_POST["pwd2"]){ 
+            $_SESSION['error'] = 'รหัสผ่านไม่ตรงกัน';
+            renderView('register_get');
+            exit();
+        }
+
+       // $result = getUser();
+        foreach($result as $row){
+            if($row['email'] == $_POST['email']){
+                $_SESSION['error'] = 'มีผู้ใช้ที่อยู่อีเมลนี้แล้ว';
+                renderView('register_get');
+                exit();
+            }
+        }
+     }
+       // register_User($_POST['fname'], $_POST['lname'], $_POST['gender'], $_POST['phone'], $_POST['role'], $_POST['bod'], $_POST['email'],$_POST['pwd'],$_POST['saj'],$_POST['rd']);
+        renderView('register_get',['data'=>$_POST]);
+        exit();
+    }
+    ?>
 
 
 
